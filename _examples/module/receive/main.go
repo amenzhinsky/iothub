87f03fc58af6d81bfd7c0000118d4c1767b2e489
chrishaylesnortal <chris.hayles@nortal.com>
2020-03-05 10:02:35 -0800

    Updated sample for module connection

--- _examples/module/receive/main.go
+++ _examples/module/receive/main.go
@@ -5,13 +5,19 @@ import (
 	"log"
 	"os"
 
-	"github.com/amenzhinsky/iothub/iotmodule"
-	iotmqtt "github.com/amenzhinsky/iothub/iotmodule/transport/mqtt"
+	"github.com/amenzhinsky/iothub/iotdevice"
+	iotmqtt "github.com/amenzhinsky/iothub/iotdevice/transport/mqtt"
 )
 
 func main() {
-	c, err := iotmodule.NewFromConnectionString(
-		iotmqtt.New(), os.Getenv("IOTHUB_MODULE_CONNECTION_STRING"), true,
+	cs := "HostName=myiothub.azure-devices.net;DeviceId=mydevice;ModuleId=mymodule;SharedAccessKey=MyAcc355K3y!=" // replace with primary module-specific connection string from IoT Hub
+	gwhn := os.Getenv("IOTEDGE_GATEWAYHOSTNAME") // when running on edge device
+	mgid := os.Getenv("IOTEDGE_MODULEGENERATIONID") // when running on edge device
+	wluri := os.Getenv("IOTEDGE_WORKLOADURI") // when running on edge device
+
+	c, err := iotdevice.NewModuleFromConnectionString(
+		// <transport>, <connection string>, <gateway hostname>, <module gen id>, <iotedge workload uri>, <use iotedge gateway for connection>,
+		iotmqtt.NewModuleTransport(), cs, gwhn, mgid, wluri, true, 
 	)
 	if err != nil {
 		log.Fatal(err)
