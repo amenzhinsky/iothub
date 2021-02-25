e3831e4b6fc20077c4c6abb2b0ef1f2ea335c63a
Aliaksandr Mianzhynski <amenzhinsky@gmail.com>
2021-02-25 16:03:54 +0300

    Fix module example

--- _examples/module/receive/main.go
+++ _examples/module/receive/main.go
@@ -11,13 +11,13 @@ import (
 
 func main() {
 	cs := "HostName=myiothub.azure-devices.net;DeviceId=mydevice;ModuleId=mymodule;SharedAccessKey=MyAcc355K3y!=" // replace with primary module-specific connection string from IoT Hub
-	gwhn := os.Getenv("IOTEDGE_GATEWAYHOSTNAME") // when running on edge device
-	mgid := os.Getenv("IOTEDGE_MODULEGENERATIONID") // when running on edge device
-	wluri := os.Getenv("IOTEDGE_WORKLOADURI") // when running on edge device
+	gwhn := os.Getenv("IOTEDGE_GATEWAYHOSTNAME")                                                                  // when running on edge device
+	mgid := os.Getenv("IOTEDGE_MODULEGENERATIONID")                                                               // when running on edge device
+	wluri := os.Getenv("IOTEDGE_WORKLOADURI")                                                                     // when running on edge device
 
 	c, err := iotdevice.NewModuleFromConnectionString(
 		// <transport>, <connection string>, <gateway hostname>, <module gen id>, <iotedge workload uri>, <use iotedge gateway for connection>,
-		iotmqtt.NewModuleTransport(), cs, gwhn, mgid, wluri, true, 
+		iotmqtt.NewModuleTransport(), cs, gwhn, mgid, wluri, true,
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -29,23 +29,15 @@ func main() {
 	}
 
 	// subscribe to events
-	sub, err := c_module.SubscribeEvents(context.Background())
+	sub, err := c.SubscribeEvents(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	go printMsgs(sub)
-	select {}
-}
-
-func printMsgs(sub *iotmodule.EventSub) {
-	msgs := sub.C()
-	err := sub.Err()
-	for {
-		if err != nil {
-			fmt.Printf("Sub Error:\n%s\n\n", err)
-		}
-		msg := <-msgs
-		fmt.Printf("Message:\n%s\n\n", msg.Payload)
+	for msg := range sub.C() {
+		log.Printf("Message:\n%s\n\n", msg.Payload)
+	}
+	if err := sub.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
