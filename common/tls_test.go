cc4bcd6a1c27d9f3013e2f3cac1a5d48f4c7e455
Alex Gustafsson <alexg@axis.com>
2022-06-15 15:20:06 +0200

    Add DigiCert Global G2 and Microsoft RSA Root CA 2017
    Azure will move on to new certs in 2023. As this package pins them
    they need to be updated. In order to not disrupt users, we should
    follow Azure's guidance and add the new certs as soon as possible.
    
    At some point in 2023, Azure will remove the DigiCert Baltimore
    Root, but for now it is recommended to keep.
    
    See the following URL for the announcement and updates:
    https://techcommunity.microsoft.com/t5/internet-of-things-blog/azure-iot-tls-critical-changes-are-almost-here-and-why-you/ba-p/2393169

--- common/tls_test.go
+++ common/tls_test.go
@@ -24,3 +24,25 @@ func TestRootCAs(t *testing.T) {
 	}
 	defer res.Body.Close()
 }
+
+func TestNewRootCAs(t *testing.T) {
+	// See validation steps here:
+	// https://techcommunity.microsoft.com/t5/internet-of-things-blog/azure-iot-tls-critical-changes-are-almost-here-and-why-you/ba-p/2393169
+
+	r, err := http.NewRequest(http.MethodGet, "https://g2cert.azure-devices.net", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c := &http.Client{Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{
+			ServerName: "g2cert.azure-devices.net",
+			RootCAs:    RootCAs(),
+		},
+	}}
+	res, err := c.Do(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+}
